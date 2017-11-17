ddbf08c4017b6264d268d801f7e8a9b7f0889233
NhanNguyen <nhankendu96@gmail.com>
2017-11-17 23:54:29 +0700

    add database and connect to form DangNhap. Ok

--- Thu5/Thu5/DangNhap.cs
+++ Thu5/Thu5/DangNhap.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,11 @@ namespace Thu5
         {
             InitializeComponent();
         }
-
+        SqlConnection con = DBConnecter.sqlConnector();
+        private void DangNhap_Load(object sender, EventArgs e)
+        {
+            con.Open();
+        }
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             DangKy dangky = new DangKy();
@@ -26,7 +31,81 @@ namespace Thu5
 
         private void btnDangNhap_Click(object sender, EventArgs e)
         {
-            this.Close();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("selectOne_TaiKhoan", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@userName", txtTenDangNhap.Text.Trim()));
+                cmd.Parameters.Add(new SqlParameter("@passWord", txtMatKhau.Text.Trim()));
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+
+                SqlCommand com2 = new SqlCommand("select HOTEN from TAIKHOAN where USERNAME = @USERNAME ", con);
+                com2.Parameters.AddWithValue("USERNAME", txtTenDangNhap.Text);
+                com2.CommandType = CommandType.Text;
+                SqlDataAdapter da2 = new SqlDataAdapter(com2);
+                DataTable dt2 = new DataTable();
+                da2.Fill(dt2);
+
+                if(dt.Rows[0][0].ToString() == "1")
+                {
+                    lblThongBao.Text = "Xin chào: "+ dt2.Rows[0][0].ToString();
+                }
+                else
+                {
+                    lblThongBao.Text = "Username or password is not correct.";
+                    txtMatKhau.Text = txtMatKhau.Tag.ToString();
+                    txtTenDangNhap.Focus();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối database.", "Thông báo",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //this.Close();
+        }
+
+        private void txtTenDangNhap_Enter(object sender, EventArgs e)
+        {
+            if (txtTenDangNhap.Text == txtTenDangNhap.Tag.ToString())
+            {
+                txtTenDangNhap.Text = "";
+                txtTenDangNhap.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtTenDangNhap_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTenDangNhap_Leave(object sender, EventArgs e)
+        {
+            if (txtTenDangNhap.Text.Trim() == "") //Trim() -> tra ve ban sao
+            {
+                txtTenDangNhap.Text = txtTenDangNhap.Tag.ToString();
+                txtTenDangNhap.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtMatKhau_Enter(object sender, EventArgs e)
+        {
+            if (txtMatKhau.Text == txtMatKhau.Tag.ToString())
+            {
+                txtMatKhau.Text = "";
+                txtMatKhau.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtMatKhau_Leave(object sender, EventArgs e)
+        {
+            if (txtMatKhau.Text.Trim() == "")
+            {
+                txtMatKhau.Text = txtMatKhau.Tag.ToString();
+                txtMatKhau.ForeColor = Color.Silver;
+            }
         }
     }
 }
